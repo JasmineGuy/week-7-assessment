aaf12a91b23d22b798daf140796fb0ea18ac70b3
JasmineGuy <guyjasmine.c@gmail.com>
2021-07-23 14:01:57 -0500

    added pangram solution

--- partthree.js
+++ partthree.js
@@ -54,13 +54,60 @@ let alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('')
 // console.log(alphabet)
 // pangram('howdy cowboy')
 
+const isPangram = (str) => {
+    if (str.length === 0) {
+      return false
+    }
 
+    let charMap = {
+      "a": 0, 
+      "b": 0, 
+      "c": 0, 
+      "d": 0, 
+      "e": 0, 
+      "f": 0, 
+      "g": 0, 
+      "h": 0, 
+      "i": 0, 
+      "j": 0, 
+      "k": 0, 
+      "l": 0, 
+      "m": 0, 
+      "n": 0, 
+      "o": 0, 
+      "p": 0, 
+      "q": 0, 
+      "r": 0, 
+      "s": 0,
+      "t": 0,
+      "u": 0,
+      "v": 0,
+      "w": 0,
+      "x": 0,
+      "y": 0,
+      "z": 0,
+    }
+  
+    let strAsSplitArr = str.toLowerCase().split("")
+    for (let char of strAsSplitArr) {
+      charMap[char] = charMap[char] + 1 || 1
+    }
+  
+    for (let key in charMap) {
+      if (charMap[key] === 0) {
+        return false
+      }
+    } return true
+  };
+
+  
+  console.log('pangram test:', isPangram('the quick brown fox jumps over the lazy dog'))
 
 //Runtime: 
 
 // Longest Word¶
 // Write a function, find_longest_word, that takes a list of words and returns the length of the longest one.
-let words = ['hi', 'goodbye', 'bug']
+let words = ['hi', 'goodbye', 'bug', 'fhjsrhfkjrhgudthgldtkghdhtlgh']
 
 function longestWord(list){
   let longest = 0
@@ -73,7 +120,9 @@ function longestWord(list){
     } return longest
 }
 
-longestWord(words)
+
+console.log('longest word function:', longestWord(words))
+
 
 //Runtime: O(n^2)
 // Space: O(n) 
